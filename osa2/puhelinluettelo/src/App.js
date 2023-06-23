959b1a7390d395cc0784e3b2d3163e5fc56daf7f
katsusah <katja.s.ahonen@student.jyu.fi>
2023-06-23 22:11:40 +0300

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,23 +2,30 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244' },
-    { name: 'Grace Hopper',
-      number: '0440123456' },
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-
+  const [personsToShow, setPersonsToShow] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const showPersons = (event) => { // etsii tiedot henkilön nimen perusteella
+    const filterName = event.target.value
+    const filteredPersons = persons.filterName(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
+    setPersonsToShow(filteredPersons)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
 
-  const addPerson = (event) => {
+  const addPerson = (event) => { // lisää henkilön tiedot
     event.preventDefault()
     const personObject = {
       name: newName,
@@ -28,8 +35,8 @@ const App = () => {
     console.log('uusi nimi', newName)
     console.log('uusi numero', newNumber)
 
-    if (persons.map(person => person.name).indexOf(newName) !== -1) {
-      alert(`${newName} is already added to phonebook`) // ilmoittaa, jos nimi löytyy jo puhelinluettelosta
+    if (persons.map(person => person.name).indexOf(newName) !== -1) { // ilmoittaa, jos nimi löytyy jo puhelinluettelosta
+      alert(`${newName} is already added to phonebook`)
       return
     }
 
@@ -37,10 +44,12 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input onChange={showPersons} />
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -60,7 +69,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {personsToShow.map(person =>
           <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
